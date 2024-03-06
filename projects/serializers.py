1a5c560d97b05500e3fb74899a223dfb227612c9
leonardo <leonardocaixeta07@gmail.com>
2024-03-06 14:24:25 -0300

    fix: requisito 2

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,7 +2,7 @@ from .models import Profile
 from rest_framework import serializers
 
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ["name", "github", "linkedin", "bio"]
+        fields = "__all__"
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -7,13 +6,3 @@ from .serializers import ProfileSerializer
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-
-    def get_permissions(self):
-        if self.action == 'list':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
